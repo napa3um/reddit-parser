d36a2a67d6a5fbe8b8fc2225874dad2077eda9d2
Alexander Kozaev <napa3um@gmail.com>
2016-07-04 23:12:09 +0700

    fix tabs

--- formatters.js
+++ formatters.js
@@ -3,23 +3,23 @@
 // http://stackoverflow.com/questions/7744912/making-a-javascript-string-sql-friendly
 const sqlEscStr = str => String(str).replace(/[\0\x08\x09\x1a\n\r"'\\\%]/g, char => {
 	switch (char) {
-    case "\0":
-        return "\\0";
-    case "\x08":
-        return "\\b";
-    case "\x09":
-        return "\\t";
-    case "\x1a":
-        return "\\z";
-    case "\n":
-        return "\\n";
-    case "\r":
-        return "\\r";
-    case "\"":
-    case "'":
-    case "\\":
-    case "%":
-        return "\\" + char;
+		case "\0":
+			return "\\0";
+		case "\x08":
+			return "\\b";
+		case "\x09":
+			return "\\t";
+		case "\x1a":
+			return "\\z";
+		case "\n":
+			return "\\n";
+		case "\r":
+			return "\\r";
+		case "\"":
+		case "'":
+		case "\\":
+		case "%":
+			return "\\" + char;
 	}
 });
 
