5e83156279e2d8a09f423658731da816ac68c1c9
Alexander Kozaev <napa3um@gmail.com>
2016-07-04 22:02:51 +0700

    mysql escape string

--- formatters.js
+++ formatters.js
@@ -1,5 +1,28 @@
 'use strict';
 
+// http://stackoverflow.com/questions/7744912/making-a-javascript-string-sql-friendly
+const sqlEscStr = str => String(str).replace(/[\0\x08\x09\x1a\n\r"'\\\%]/g, char => {
+	switch (char) {
+    case "\0":
+        return "\\0";
+    case "\x08":
+        return "\\b";
+    case "\x09":
+        return "\\t";
+    case "\x1a":
+        return "\\z";
+    case "\n":
+        return "\\n";
+    case "\r":
+        return "\\r";
+    case "\"":
+    case "'":
+    case "\\":
+    case "%":
+        return "\\" + char;
+	}
+});
+
 module.exports = {
 
 //------------------------------------------------------------------------------
@@ -10,7 +33,7 @@ module.exports = {
 )
 
 ${rows.map(row => `INSERT INTO ${params.table} (${fields.map(field => field.replace(/:/g, '__')).join(', ')})
-\tVALUES (${fields.map(field => `'${row[field]}'`)});
+\tVALUES (${fields.map(field => `'${sqlEscStr(row[field])}'`)});
 `).join('\n')}
 `,
 
