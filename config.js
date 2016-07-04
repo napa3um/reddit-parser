d301e87eb0fdcf70a36350b361dd1c562b5c02ee
napa3um <napa3um@gmail.com>
2016-07-04 21:10:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,22 @@
 module.exports = {
 	url: 'http://www.reddit.com/r/javascript.json',
+
+	// output fields - [prefix][field_name][postfix]
+	// prefix "+" - sort ascending
+	// prefix "-" - sort descending
+	// postfix ":sum" - aggreagate, sum
+	// postfix ":count" - aggreagate, count (may be without field name)
+	// postfix ":min" - aggreagate, min
+	// postfix ":max" - aggreagate, max
 	fields: ['domain', ':count', '-score:sum'],
 
+	// simple formatter, all values is strings
 	formatter: {  // name and any other params
 		name: 'cvs',
 		separator: ', '
 	}
 
+	// other simple formatter, all values is strings
 	// formatter: {  // name and any other params
 	// 	name: 'sql',
 	// 	table: 'reddit_pages'
